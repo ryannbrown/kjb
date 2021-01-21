d97fbf2c3c96a8a5f0ba0b7589d216243bdf4ee1
ryannbrown <44414542+ryannbrown@users.noreply.github.com>
2021-01-20 20:03:26 -0500

    about section update

--- client/src/pages/About/index.js
+++ client/src/pages/About/index.js
@@ -48,34 +48,28 @@ export default class About extends Component {
                   top: '35%',
                 }}
               >
-                {/* <Image className="about-img" width="400px" src={mill}></Image> */}
-                {/* <Image className="about-logo" src={pinkLogo}></Image> */}
               </div>
             </div>
           </div>
           <div className="pink-block">
             <div className="narrow">
-              {/* <div className="about-greeting">
-              <h1>Hi There!</h1>
-              <p>
-                Welcome to my blog of creative work. My name is Millicent, but I
-                go by Millie. I am from Charlotte, North Carolina, and I work
-                remotely in Northern California as a full-time illustrator. I am
-                passionate about writing, illustration, art and fashion. This
-                serves as a place for me to show you the person behind my
-                writing and designs, and I hope that you will feel inspired by
-                it. Thank you for your time, and I look forward to sharing with
-                you a more intimate look at my creations. Sincerely, Millie
-              </p>
-            </div> */}
             </div>
             <div className="about">
-              <h1>Hi There!</h1>
+              <h1>A little bit about me!</h1>
               <p>
-              I am all about the inward journey, and experiencing the freedom that can be found when we make peace with our shadows.
-Based out of Raleigh, NC. I have a Social Work degree from UNC Willmington.
-I am certified in Reiki 2, a Shamanic Practitioner, Certified Health Coach, and Shadow Worker.
+              Being highly sensitive and intuitive, I've been reading energy for as long as I can remember. My personal path led me into the fields of spirituality, psychology, and integrative nutrition. Before I discovered holistic and integrative healing, I was stuck on the relentless self-improvement hamster wheel. I had no idea how deeply I had repressed emotions, or the amount of trauma that had affected me. I had no idea what shadow work was or how the subconscious mind was running the show. I had no idea that physical symptoms were rooted in energetic imbalances. I tried many traditional approaches to healing. I would feel better for a time, but old patterns and stories would inevitably resurface. After an incredibly dark night of the soul, I had a personal spiritual awakening that changed everything. This led me on a deep journey of exploration- I expanded my mind, dropped the dogma, let go of my resistance, and dove headfirst into the shadow of my subconscious. This is is where the deep and restorative healing began. I became well acquainted with my own energy field, the stories it was holding, and the imbalances that needed healing. I discovered the inseparable connection between the mind, body, and spirit. I discovered that when I changed my energy, I changed my whole life. No more recurring, unwanted patterns. No more running from emotions and triggers. No more persistent, underlying anxiety and instability. Energy is the root and foundation of everything. Healing on an energetic level healed my inner world and recalibrated my entire state of being. I was no longer at war within myself. I had transformed my inner world into a complete oasis. It is my personal mission to empower others in their own healing and guide them into a felt sense of wholeness and freedom.
               </p>
+<p>I've been immersed in the healing arts for years and have been trained and certified in several wonderful healing modalities. For my life's work, I've decided to put together the ultimate healing cocktail. Using a combination of my own energetic abilities and my years of training, I intuitively design sessions and packages for clients that deliver lasting transformation and results. </p>
+<p>As a Certified Shamanic Practitioner and Reiki Master, I sense the energy field of my client, uncover blocks and imbalances, and work to restore harmony and healing within the chakras and auric field. </p>
+<p>As a Certified Integrative Health Coach, I work with clients to create lifestyle changes that will do wonders for the physical body and create lasting change in overall health and wellness.</p>
+<p>As a Certified Meditation teacher, I give clients the tools and techniques to dive into deep states of consciousness that will transform the mind, develop the skill of observation, and bring about stillness and quietness.</p>
+<p>
+As a Certified Kundalini Yoga teacher, I am able to give clients powerful movement practices as a somatic release to facilitate the process of deep healing.</p>
+<p>As a shadow worker, I am able to hold space for clients to work through their darkest fears and most crippling wounds. Providing a safe space for clients to process difficult memories and emotions, I offer my unconditional presence, compassion, and thought-provoking questions and guidance.</p>
+<p>
+It is my honor to hold sacred space for your healing and transformation. This work is my utmost passion. Every session is done in compassion, integrity, and excellence. May you break free from your unwanted patterns, experience the freedom that is inherently yours, and feel fully whole and empowered. May you discover the haven within.</p>
+<p>Xo
+Kathryn</p>
             </div>
           </div>
           <div className="white-block">
