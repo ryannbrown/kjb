ee3be575dc95197393fe8655819af98119ba04e7
ryannbrown <44414542+ryannbrown@users.noreply.github.com>
2021-01-27 21:27:32 -0500

    teste fixhes

--- client/src/components/Testimonials/index.js
+++ client/src/components/Testimonials/index.js
@@ -65,7 +65,7 @@ export default class Testimonials extends Component {
                   <FontAwesomeIcon className="quote-icon" icon={faQuoteLeft}></FontAwesomeIcon>
                   <div className="off-description">
                     <p className="off-price">
-                    Kathryn did a beautiful job of connecting with me through energy healing. Without prompting she immediately focused on my second (sacral) chakra which is the same area where I’ve been having undiagnosed chronic back/hip pain. Her insight and feedback has given me numerous directions in which I intend to continue growing. I am so thankful for the opportunity I had to work with her and am excited to continue my journey towards healing!
+                    As a shamanic practitioner myself I am very picky about who I see for my own personal healing sessions. Kathryn is the real deal. She creates this beautiful womb of safe sacred space in which to totally release and relax. Which is essential for deep healing!  The space she creates and brings to sessions is judgement free, warm, supportive, and loving. Sessions with Kathryn are very relaxing while still being a very powerful release and healing. If you are looking for a relaxing, warm, understanding shamanic healing experience seek no further. 
                     </p>
                     <p className="off-title">⁃	Beatrice K</p>
                   </div>
